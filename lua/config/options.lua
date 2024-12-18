26cb10867e3079c13fa600ed1c2bbc14646091c3
Taylor Johnson <johnsoctbus@gmail.com>
2024-12-18 07:31:56 -0500

    init

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -11,9 +11,9 @@ vim.opt.mouse = ""
 vim.opt.autoindent = true -- Enable automatic indentation
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.smartindent = true -- Automatically indent new lines
-vim.opt.shiftwidth = 8 -- Number of spaces used for each indentation step
-vim.opt.softtabstop = 8
-vim.opt.tabstop = 8 -- number of spaces a tab counts for
+vim.opt.shiftwidth = 4 -- Number of spaces used for each indentation step
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4 -- number of spaces a tab counts for
 
 -- Enable yanking to the system clipboard
 vim.opt.clipboard = "unnamedplus"
