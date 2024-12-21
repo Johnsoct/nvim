4f9a0bd96f4954ccbd4b143a51c0bcaff2c61f09
Taylor Johnson <johnsoctbus@gmail.com>
2024-12-20 23:00:38 -0500

    telescope done (for now)

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,40 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
+        config = function()
+            local ts = require("telescope")
+            local tsbiff = require("telescope.builtin").find_files
+            local tsbiht = require("telescope.builtin").help_tags
+
+            -----------------------
+            -- Telescope options --
+            -----------------------
+            ts.setup({
+                extensions = {
+                    fzf = {},
+                },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown",
+                    },
+                },
+            })
+
+            -----------------------
+            -- Telescope keymaps --
+            -----------------------
+            vim.keymap.set("n", "<space>fh", tsbiht)    -- find help documentation
+            vim.keymap.set("n", "<space>fd", tsbiff)    -- find within cwd
+            vim.keymap.set("n", "<space>en", function() -- find neovim config files
+                tsbiff({
+                    cwd = vim.fn.stdpath("config"),
+                })
+            end)
+        end,
+    },
+}
