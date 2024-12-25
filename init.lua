c3e36afb8dfedf92ac2be28a06c393bf2b1f9f0f
Taylor Johnson <johnsoctbus@gmail.com>
2024-12-25 09:17:16 -0500

    added todo highlighting and folding settings

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -32,3 +34,15 @@ vim.g.loaded_netrwPlugin = 1
 
 -- Optionally enabled 24-bit colour
 vim.opt.termguicolors = true
+
+---------------
+--- FOLDING ---
+---------------
+vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+    pattern = "*",
+    callback = function()
+        vim.defer_fn(function()
+            vim.cmd("normal! zR")
+        end, 10) -- Delay slightly (10ms) to ensure folds are calculated
+    end,
+})
