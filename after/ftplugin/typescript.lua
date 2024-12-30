63948bcbcaded40c7384e4395c1638d8dd3c9a5f
Chris Johnson <johnsoctbus@gmail.com>
2024-12-30 18:34:33 -0500

    updated tab spacing

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,3 +1,8 @@
 local set = vim.opt_local
 
-set.shiftwidth = 4
+vim.opt.autoindent = true  -- Enable automatic indentation
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Automatically indent new lines
+vim.opt.shiftwidth = 4     -- Number of spaces used for each indentation step
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4        -- number of spaces a tab counts for
--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
-vim.opt.shiftwidth = 4
+vim.opt.autoindent = true  -- Enable automatic indentation
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Automatically indent new lines
+vim.opt.shiftwidth = 4     -- Number of spaces used for each indentation step
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4        -- number of spaces a tab counts for
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.updatetime = 250
