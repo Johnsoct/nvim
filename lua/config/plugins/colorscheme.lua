8998a9250e23710256d525b4f3b631638985d5f7
Taylor Johnson <johnsoctbus@gmail.com>
2025-01-02 08:11:27 -0500

    damn; theme is fine; my config is creating conflicts

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -5,10 +5,23 @@ return {
         priority = 1000,
         config = function()
             vim.opt.termguicolors = true
+
+            local transparent = true
+
+            -- if transparent then
+            --     vim.cmd([[
+            --         highlight Normal guibg=none
+            --         highlight NonText guibg=none
+            --         highlight Normal ctermbg=none
+            --         highlight NonText ctermbg=none
+            --     ]])
+            -- end
+ 
             require('cyberdream').setup({
                 theme = {
                     saturation = 0,
                 },
+                transparent = transparent,
             })
             vim.cmd.colorscheme("cyberdream")
         end,
