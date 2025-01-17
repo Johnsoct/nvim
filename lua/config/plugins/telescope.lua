48cad281081888ce5093ae2462420adc4abf43b7
Taylor Johnson <johnsoct@hey.com>
2025-01-17 07:16:51 -0500

    improved and reorganize telescope keybinds

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -8,21 +8,24 @@ return {
         },
         config = function()
             local ts = require("telescope")
-            local tsbiff = require("telescope.builtin").find_files
-            local tsbiht = require("telescope.builtin").help_tags
-            local tsgs = require("telescope.builtin").grep_string
 
             -----------------------
             -- Telescope options --
             -----------------------
             ts.setup({
-                defaults = {
-                    file_ignore_patterns = {
-                        "^node_modules/",
-                    },
-                },
+                -- defaults = {
+                --     file_ignore_patterns = {
+                --         "^node_modules/",
+                --     },
+                -- },
                 extensions = {
-                    fzf = {},
+                    fzf = {
+                        -- Default options
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    },
                 },
                 pickers = {
                     find_files = {
@@ -32,16 +35,36 @@ return {
             })
 
             -----------------------
+            -- Extension Loading --
+            -----------------------
+            require("telescope").load_extension("fzf")
+
+            -----------------------
             -- Telescope keymaps --
             -----------------------
-            vim.keymap.set("n", "<space><space>", tsbiht) -- find help documentation
-            vim.keymap.set("n", "<space>fs", tsgs)        -- find help documentation
-            vim.keymap.set("n", "<space>fd", tsbiff)      -- find within cwd
-            vim.keymap.set("n", "<space>en", function()   -- find neovim config files
-                tsbiff({
+            local tsff = require("telescope.builtin").find_files
+            local tsht = require("telescope.builtin").help_tags
+            local tsgs = require("telescope.builtin").grep_string
+            local tslg = require("telescope.builtin").live_grep
+            local tsbuffers = require("telescope.builtin").buffers
+            local tsoldfiles = require("telescope.builtin").oldfiles
+
+            --en*
+            vim.keymap.set("n", "<space>en", function()
+                tsff({
                     cwd = vim.fn.stdpath("config"),
                 })
-            end)
+            end, { desc = "telesceope neovim config files" })
+
+            --f*
+            vim.keymap.set("n", "<space><space>", tsht, { desc = "telescope help tags" })
+            vim.keymap.set("n", "<space>fb", tsbuffers, { desc = "telescope open buffers" })
+            vim.keymap.set("n", "<space>ff", tsff, { desc = "telescope find files" })
+            vim.keymap.set("n", "<space>fg", tslg, { desc = "telescope live grep" })
+            vim.keymap.set("n", "<space>fo", tsoldfiles, { desc = "telescope open old files" })
+            vim.keymap.set("n", "<space>fs", tsgs, { desc = "telescope grep string" })
+
+            --g*
             vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, { noremap = true, silent = true })
         end,
     },
